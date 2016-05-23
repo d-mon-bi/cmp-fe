225c7c53680d49abb090a008b4200e7794a47a86
d-mon-bi <daniel.montero+gh@gmail.com>
2016-05-23 10:20:43 -0600

    Added google maps and markers

--- js/searchbox.js
+++ js/searchbox.js
@@ -1,5 +1,5 @@
 apiURL  = "http://52.40.59.4:3000/"
-
+var markers = [];
 function toggleSearchUI()
 {
 	isVisible = $("#clickbox").is(':visible');
@@ -9,6 +9,7 @@ function toggleSearchUI()
 		$("#searchresults").hide();
 		$("#breadcrumbs-box").hide();
 		$("#searchUIToggle").text(">>")
+		$("#search").width(25);
 	}
 	if (!isVisible)
 	{
@@ -16,8 +17,31 @@ function toggleSearchUI()
 		$("#searchresults").show();
 		$("#breadcrumbs-box").show();
 		$("#searchUIToggle").text("<<")
+		$("#search").width("55%");
 	}
 	
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
 }
 
 function filterCategory(category)
@@ -29,11 +53,17 @@ function filterCategory(category)
 	})
 	.done (
 		function ( results) {
+			
 			$("#clickbox-list").empty();
 			$.each(results, function(i, profession){
 				$("#clickbox-list").append( '<li class="clickbox-item" id="cb-' + profession.name.replace(/\s+/g, '') + '" onClick="filterCategory(\''+profession.name+'\')"> <a>' +profession.name+'</a></li>')
 			});
-	});
+	})
+	.fail (
+		function(jqXHR, textStatus, errorThrown){
+			
+		}
+	);
 	searchProfession(category)
 	addBreadcrumb(category)
 }
@@ -45,13 +75,41 @@ function searchProfession(profession)
 		dataType : "json",
 		url : searchURL,
 	})
-	.done (
-		function ( results) {
-			$("#searchresults-list").empty();
-			$.each(results, function(i, professional){
-				$("#searchresults-list").append( '<li class="search-item" id="sr-' + professional._id + '"> <a>' +professional.name + " " + professional.lastName+'</a></li>')
+	.done (function ( results) {
+		var map = document.getElementById('map').gMap;
+		clearMarkers();
+		$("#searchresults-list").empty();
+		$.each(results, function(i, professional){
+			$("#searchresults-list").append( '<li class="search-item" id="sr-' + professional._id + '"> <a>' +professional.name + " " + professional.lastName+'</a></li>');
+			$.each(professional.offices, function(j, office){
+				
+				var infowindow = new google.maps.InfoWindow({
+					content: formatOffice(professional, office)
+				});
+				var pos = {lat:office.lat, lng: office.lon};
+				var marker = new google.maps.Marker({
+					position:pos,
+					map: map,
+					title: professional.name + " " + professional.lastName
+				});
+				marker.addListener('mouseover', function() {
+					infowindow.open(map, marker);
+				});
+				marker.addListener('mouseout', function() {
+					infowindow.close();
+				});
+				markers.push(marker);
 			});
+		});
 	});
+}
+
+function formatOffice(professional, office)
+{
+	formattedOffice = '';
+	formattedOffice = formattedOffice +  '<div class="markerInfoWindow"><h1>'+professional.name + " " + professional.lastName+'</h1><h2>'+office.name+'</h2><h3>'+office.phone+'</h3></div>';
+	
+	return formattedOffice;
 }
 
 function addBreadcrumb(category)
